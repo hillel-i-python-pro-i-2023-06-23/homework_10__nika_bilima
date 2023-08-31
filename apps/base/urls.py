9cae1ad87ebaedf755daf6f04f0d08bd64f62177
nikusion <bilima.nika2003@gmail.com>
2023-08-31 17:20:39 +0300

    delete apps.urls

--- apps/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserGeneratorView, HomePageView
-
-app_name = "base"
-
-urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
-    path("generate/", UserGeneratorView.as_view(), name="generate_users"),
-]
